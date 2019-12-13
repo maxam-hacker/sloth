73149e0fc4ae0a8dbdf592eb5763494f8b1b8e75
Maxam Hacker <maxam.hacker@gmail.com>
2019-12-13 14:15:07 +0300

    debug

--- src/com/maxamhacker/theseniorprogrammer/TheSeniorEndpoints.java
+++ src/com/maxamhacker/theseniorprogrammer/TheSeniorEndpoints.java
@@ -37,6 +37,7 @@ public class TheSeniorEndpoints extends HttpRequestProcessor {
 		} else if (path.startsWith("/codehacker")) {
 			if (params == null) 
 				return;
+			System.out.println(params.get("topic"));
 			response.setStatus(HttpResponseStatus.OK);
 			response.setBody("spring-framework, spring-boot, spring-data-commons");
 		}
